52b95c0e82456cf95710b17a83075468658033f0
jordan <jordan@jordans-Air.lan>
2019-12-10 22:09:51 +0700

    got form and reducer displaying

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import { reducer, initialState } from './reducers/Reducer';
+import Form from './components/Form';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItem = (e, item) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', payload: item });
+  };
+
+  const clearCompleted = e => {
+    e.preventDefault();
+    dispatch({ type: 'CLEAR_COMPLETE' });
+  };
+
   return (
    <div className='App'>
      <h1>Reducer Project</h1>
+     <Form addItem={addItem} />
     </div>
   );
 }
